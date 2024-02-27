544b6f943de54f8c6ce27b295fc9c2df28edd930
Sritharan Thiyagarajah <sk.sritharan75@gmail.com>
2024-02-27 21:03:39 +0530

    Added Bottom Tab

---
+++ app/screens/DiscoverScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function DiscoverScreen() {
+  return (
+    <View>
+      <Text>DiscoverScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
