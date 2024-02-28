9e6b63516a916bb8305b794d6baf2e4d54c895f3
Sritharan Thiyagarajah <sk.sritharan75@gmail.com>
2024-02-28 11:11:22 +0530

    Added Filters & discovering

---
+++ app/components/Filters.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View, Pressable } from "react-native";
+
+import { TAGS } from "../data/appData";
+
+export default function Filters({ onTagSelected }) {
+  const [selectedTag, setSelectedTag] = useState("");
+
+  const renderItem = ({ item }) => (
+    <Pressable
+      style={[styles.tag, selectedTag === item.value && styles.selectedTag]}
+      onPress={() => {
+        setSelectedTag(selectedTag === item.value ? "" : item.value);
+        onTagSelected(item.value);
+      }}
+    >
+      <Text
+        style={[styles.text, selectedTag === item.value && styles.selectedText]}
+      >
+        {item.name}
+      </Text>
+    </Pressable>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        data={TAGS}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+  },
+  tag: {
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+    margin: 2,
+    borderWidth: 1,
+    borderRadius: 16,
+    borderColor: "black",
+  },
+  selectedTag: {
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    letterSpacing: 1,
+    color: "black",
+  },
+  selectedText: {
+    color: "white",
+  },
+});
--- app/screens/DiscoverScreen.js
+++ app/screens/DiscoverScreen.js
@@ -1,12 +1,69 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
+
+import Filters from "../components/Filters";
+import { getQuotesByTag } from "../services/apiService";
+import Quote from "../components/Quote";
+import Loader from "../components/Loader";
 
 export default function DiscoverScreen() {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  async function fetchQuotes(tag) {
+    setIsLoading(true);
+    const quotes = await getQuotesByTag(tag);
+    setQuotes(quotes);
+    setIsLoading(false);
+  }
+
   return (
-    <View>
-      <Text>DiscoverScreen</Text>
+    <View style={styles.container}>
+      <Filters onTagSelected={fetchQuotes} />
+      {isLoading ? (
+        <Loader />
+      ) : quotes.results && quotes.results.length > 0 ? (
+        <FlatList
+          data={quotes.results}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => <Quote quote={item} />}
+          contentContainerStyle={styles.flatListContent}
+        />
+      ) : (
+        <Text style={styles.noResultsText}>
+          No quotes found for selected tag.
+        </Text>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  flatListContent: {
+    paddingBottom: 16,
+  },
+  noResultsText: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 16,
+    color: "gray",
+  },
+});
